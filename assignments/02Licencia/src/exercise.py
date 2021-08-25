925c09b1c7bd13215aaaaedcba8fd7d5f57b4c60
Mike-GB <88850952+Mike-GB@users.noreply.github.com>
2021-08-25 12:14:34 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,15 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    if edad < 18:
+        print("No cumples requisitos")
+    if edad >= 18:
+        id = str(input("¿Tienes identificación oficial?" (s/n): "))
+                       if id == n:
+                        print ("No cumples requisitos")
+                       if id == s:
+                        print("Trámite de licencia concedido")
 
 
 if __name__ == '__main__':
