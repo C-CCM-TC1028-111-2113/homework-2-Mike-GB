756fdde6b18c788f55150fc274c18f415b8de57b
Mike-GB <88850952+Mike-GB@users.noreply.github.com>
2021-08-25 11:52:23 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,12 +1,12 @@
 import math
 def main():
     x=int(input("Dame un número: "))
-if x == 0:
-     print("Es cero")
-if x < 0:
-     print("Es negativo")
-if x > 0:
-     print("Es positivo")
+    if x == 0:
+        print("Es cero")
+    if x < 0:
+        print("Es negativo")
+    if x > 0:
+        print("Es positivo")
     
 
 if __name__ == '__main__':
